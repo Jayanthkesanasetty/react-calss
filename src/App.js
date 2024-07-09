9045947154d8790a242558af74b590a64fe4f98b
Jayanth <208x1a435@khitguntur.ac.in>
2024-07-09 18:21:25 +0530

    table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+// import './components/button/table/table.css'
 
+// import But from './components/button/button';
+// import Main,{Head2,Head3} from './components/button/heading';
+import Table from './components/button/table/table';
 function App() {
   return (
     <div className="App">
-      <h1>Jayanth</h1>
+      <Table />
+      {/* <But />
+      <But />
+      <But />
+      <But />
+      <But />
+      <But />
+      <But />
+      <Head2 />
+      <Head2 />
+      <Head2 />
+      <Head3 />
+      <Head3 /> */}
     </div>
-  );
+  );  
 }
 
 export default App;
---
+++ src/components/button/table/table.js
@@ -0,0 +1,27 @@
+import './table.css'
+const Table = ()=>{
+    return (<table >
+        <tr >
+            <th >S.no</th>
+            <th >Name</th>
+            <th >Mobile</th>
+        </tr>
+        <tr >
+            <td >1</td>
+            <td >Kishore</td>
+            <td >12345</td>
+        </tr>
+        <tr >
+            <td >2</td>
+            <td >Rakesh</td>
+            <td >51232</td>
+        </tr>
+        <tr >
+            <td >3</td>
+            <td >Kishore</td>
+            <td >12345</td>
+        </tr>
+    </table>)
+}
+
+export default Table ;
