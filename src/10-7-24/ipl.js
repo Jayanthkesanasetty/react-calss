c6753a020e7fa8141c1f67cbec4fea1ebb0d8841
Jayanth <208x1a435@khitguntur.ac.in>
2024-07-11 11:56:38 +0530

    commit

---
+++ src/10-7-24/ipl.js
@@ -0,0 +1,13 @@
+// import IplTeam from "./iplteam";
+
+// const Ipl=()=>{
+//     return (
+//         <>
+//         {IplTeam.map((EachTeam)=>(
+//         <h1>{EachTeam}</h1>   
+//         ))}
+//         </>
+//     )
+// }
+
+// export default Ipl;
--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import './App.css';
 // import Main,{Head2,Head3} from './components/button/heading';
 import Table from './components/table/table';
 import Card ,{Card2} from './components/cards/cards';
+import IndiaIpl from './10-7-24/iplteam';
+import Ipl from './10-7-24/ipl';
+import Main from './components/button/heading';
 function App() {
   return ( 
     <div className="App">
       {/* <h1>TABLE</h1>
       <Table />
       <h1>CARDS</h1>
-      <Card /> */}
-      <Card2 />
+      <Card />
+      <Card2 /> */}
+      <IndiaIpl />
+      {/* <IplTeam />
+      <Ipl /> */}
+    {/* <Ipl /> */}
     </div>
   );  
 }
